535e745481145ca3548de84b89e6ad2d95ab3993
Damien Elmes <gpg@ankiweb.net>
2020-10-24 10:38:24 +1000

    use svelte-preprocess prior to compiling
    Allows usage of typescript; type checking still requires separate
    call to svelte-check.

--- internal/svelte.js
+++ internal/svelte.js
@@ -6,10 +6,30 @@ const input = process.argv[2];
 const svelte = require("svelte/compiler");
 
 const source = fs.readFileSync(input, "utf8");
-const result = svelte.compile(source, {
-  format: "esm",
-  generate: "dom",
-  filename: outputJs
-});
 
-fs.writeFileSync(outputJs, result.js.code);
+const preprocessOptions = require("svelte-preprocess")({});
+preprocessOptions.filename = input;
+
+svelte.preprocess(source, preprocessOptions).then(
+  (processed) => {
+    let result;
+    try {
+      result = svelte.compile(processed.toString(), {
+        format: "esm",
+        generate: "dom",
+        filename: outputJs,
+      });
+    } catch (err) {
+      console.log(`compile failed: ${err}`);
+      return;
+    }
+    if (result.warnings.length > 0) {
+      console.log(`warnings during compile: ${result.warnings}`);
+      return;
+    }
+    fs.writeFileSync(outputJs, result.js.code);
+  },
+  (error) => {
+    console.log(`preprocess failed: ${error}`);
+  }
+);
